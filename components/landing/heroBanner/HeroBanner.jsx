14dd2eeab9c36315d838087fbfb7a1f5e6fa306e
Onkar k <onkarok0273.1@gmail.com>
2024-10-22 19:12:20 +0530

    adding hero img

--- components/landing/heroBanner/HeroBanner.jsx
+++ components/landing/heroBanner/HeroBanner.jsx
@@ -1,26 +1,51 @@
-
+import { H1 } from "@/components/typography/Typography";
 import Image from "next/image";
 
 export default function HeroBanner() {
-    return (
-      <div className="relative" >
-
-      <div className="h-[550px] bg-gray-400" >
-  
+  return (
+    <div className="relative">
+      <div className="h-[550px] bg-gray-400">
         {/* web image */}
-  
-        <Image src="/images/landing/test2.jpg" alt="heroWeb" width={1280} height={550}    className="hidden md:block  h-full w-full object-cover object-center" /> 
-  
+
+        <Image
+          src="/images/landing/webHero.jpg"
+          alt="heroWeb"
+          width={1280}
+          height={550}
+          className="hidden md:block  h-full w-full object-cover object-center"
+        />
+
         {/* mobile image */}
-  
-        <Image src="/images/landing/test3.jpg" alt="heroMob" width={390} height={550}     className="md:hidden  h-full w-full object-cover object-center" />
+
+        <Image
+          src="/images/landing/mobHero.jpg"
+          alt="heroMob"
+          width={390}
+          height={550}
+          className="md:hidden  h-full w-full object-cover object-center"
+        />
       </div>
-        <h1 className={'hidden md:block absolute top-[5%] left-[5%] md:top-[47%] md:left-[10%] text-accent text-h1 font-semibold p-2.5'} >
-          <span>Celebrate Ganpati guilt free</span> <br></br> with Shidori!
-        </h1>
-        <h1 className={'md:hidden absolute top-[5%] left-[5%] text-accent text-h1 font-semibold p-2.5'} >
-          Celebrate <br></br> Ganpati guilt free <br></br> with Shidori!
-        </h1>
-      </div>
-    )
-  }
+
+      {/* web text */}
+      <h1
+        className={
+          "hidden md:block absolute top-10 left-1/2 transform -translate-x-1/2 text-accent text-h1 font-semibold"
+        }
+      >
+        Homemade - wholesome - tassty potly of goodness.
+      </h1>
+
+      {/* mob text */}
+
+      <h1
+        className={
+          "md:hidden absolute top-3 left-3 text-accent text-h1 font-semibold "
+        }
+      >
+        <p>Homemade</p>
+        <p>wholesome</p>
+        <p>tassty potly of goodness.</p>
+      </h1>
+    </div>
+  );
+}
